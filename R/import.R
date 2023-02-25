0076d3f31e6e4a376d5cb057e25d253217fefad7
taha@asks2 <taha@chepec.se>
2023-02-25 03:46:21 +0100

    Correctly handle input data file with multiple intervals.
    + improved function description
    + bumped version patch number

--- R/import.R
+++ R/import.R
@@ -1,7 +1,10 @@
 #' Import data from XPS-ESCA spectrometer
 #'
-#' Read the normalised and calibrated data from ASCII text-file
-#' containing intensities, and calculate energy steps.
+#' Import ASCII text-file with one or more intervals of XPS data,
+#' where each interval (transition) specifies a description,
+#' x-start (binding energy), x-step, and number of rows.
+#' These ASCII files commonly use the *.asc extension, but may use
+#' other extensions.
 #'
 #' @param xps.path Path to file with XPS data in expected format
 #'
@@ -46,10 +49,10 @@ xps2df <- function(xps.path) {
    intervals$begin.data <- as.numeric(NA)
    intervals$end.data   <- as.numeric(NA)
    for (i in 1:dim(intervals)[1]) {
-      intervals$desc[i] <- f[intervals$begin[i]:intervals$end[i]][3]
+      intervals$desc[i]   <- f[intervals$begin[i]:intervals$end[i]][3]
       intervals$xstart[i] <- as.numeric(f[intervals$begin[i]:intervals$end[i]][5])
-      intervals$xstep[i] <- as.numeric(f[intervals$begin[i]:intervals$end[i]][6])
-      intervals$xlen[i] <- as.numeric(f[intervals$begin[i]:intervals$end[i]][7])
+      intervals$xstep[i]  <- as.numeric(f[intervals$begin[i]:intervals$end[i]][6])
+      intervals$xlen[i]   <- as.numeric(f[intervals$begin[i]:intervals$end[i]][7])
    }
    intervals$begin.data <- intervals$begin + 7
    intervals$end.data   <- intervals$begin.data + intervals$xlen - 1
@@ -69,32 +72,34 @@ xps2df <- function(xps.path) {
          #    sep = "-"),
          spectrum = common::int2padstr(ii = 1, pchr = "0", w = 3),
          interval = intervals$desc[1],
-         xstart = intervals$xstart[1],
-         xstep = intervals$xstep[1],
+         xstart   = intervals$xstart[1],
+         xstep    = intervals$xstep[1],
          x = seq(from = intervals$xstart[1], by = intervals$xstep[1], length.out = intervals$xlen[1]),
          y = as.numeric(f[intervals$begin.data[1]:intervals$end.data[1]]))
 
    if (dim(intervals)[1] > 1) {
       for (i in 2:dim(intervals)[1]) {
          data.exp <-
-            data.frame(
-               # substrateid = sampleid,
-               # sampleid = paste(
-               #    sampleid,
-               #    # use the last two digits in the filename as unique specifier
-               #    gsub(
-               #       pattern = "^.+_", replacement = "",
-               #       x = gsub(
-               #          pattern = paste0("\\.", xps.ext, "$"),
-               #          replacement = "",
-               #          x = basename(xps.path))),
-               #    sep = "-"),
-               spectrum = common::int2padstr(ii = 1, pchr = "0", w = 3),
-               interval = intervals$desc[i],
-               xstart = intervals$xstart[i],
-               xstep = intervals$xstep[i],
-               x = seq(from = intervals$xstart[i], by = intervals$xstep[i], length.out = intervals$xlen[i]),
-               y = as.numeric(f[intervals$begin.data[i]:intervals$end.data[i]]))
+            rbind(
+               data.exp,
+               data.frame(
+                  # substrateid = sampleid,
+                  # sampleid = paste(
+                  #    sampleid,
+                  #    # use the last two digits in the filename as unique specifier
+                  #    gsub(
+                  #       pattern = "^.+_", replacement = "",
+                  #       x = gsub(
+                  #          pattern = paste0("\\.", xps.ext, "$"),
+                  #          replacement = "",
+                  #          x = basename(xps.path))),
+                  #    sep = "-"),
+                  spectrum = common::int2padstr(ii = i, pchr = "0", w = 3),
+                  interval = intervals$desc[i],
+                  xstart   = intervals$xstart[i],
+                  xstep    = intervals$xstep[i],
+                  x = seq(from = intervals$xstart[i], by = intervals$xstep[i], length.out = intervals$xlen[i]),
+                  y = as.numeric(f[intervals$begin.data[i]:intervals$end.data[i]])))
       }
    }
    # data.exp$idunique <- paste(data.exp$sampleid, data.exp$spectrum, sep = "-")
